efc9bf7841a76ce5c42d1900ff755101e543289c
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-11 23:04:48 +0200

    Changes in controller

--- GithubProfile/src/AppBundle/Controller/GithubController.php
+++ GithubProfile/src/AppBundle/Controller/GithubController.php
@@ -14,6 +14,45 @@ class GithubController extends Controller
 
     public function githubAction(Request $request)
     {
-        return $this->render('github/index.html.twig');
+        return $this->render('github/index.html.twig', [
+            'avatar_url' => 'https://avatars3.githubusercontent.com/u/27738202?v=3',
+            'name' => 'Rafał',
+            'login' => 'rafalratajczyk',
+            'details' => [
+                'location' => 'Wroclaw, Poland',
+                'joined_on' => 'Joined on 18th April 2017'
+            ],
+            'linkedIn' => 'https://www.linkedin.com/in/rafał-ratajczyk',
+            'social_data' => [
+                'public_repos' => 6,
+                'public_gists' => 0,
+                'followers' => 0,
+                'following' => 0,
+            ],
+        ]);
     }
+
+    /**
+     * @Route("/profile", name="profile")
+     */
+
+//    public function profileAction(Request $request)
+//    {
+//        return $this->render('github/profile.html.twig', [
+//            'avatar_url' => 'https://avatars3.githubusercontent.com/u/27738202?v=3',
+//            'name' => 'Rafał',
+//            'login' => 'rafalratajczyk',
+//            'details' => [
+//                'location' => 'Wroclaw, Poland',
+//                'joined_on' => 'Joined on 18th April 2017'
+//            ],
+//            'linkedIn' => 'https://www.linkedin.com/in/rafał-ratajczyk',
+//            'social_data' => [
+//                'public_repos' => 6,
+//                'public_gists' => 0,
+//                'followers' => 0,
+//                'following' => 0,
+//            ],
+//        ]);
+//    }
 }
